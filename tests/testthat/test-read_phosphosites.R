a57837d4655724c6c08ab9dcf82d8f92bc3e8453
Cas Blaauw <38132585+CasBlaauw@users.noreply.github.com>
2022-02-01 01:15:50 +0100

    Add tests for phosphosite read fns

---
+++ tests/testthat/test-read_phosphosites.R
@@ -0,0 +1,15 @@
+phosphosite_path <- system.file('extdata', 'Phosphorylation_Site_Dataset_head', package = 'phosphocie')
+kinsub_path <- system.file('extdata', 'Kinase_Substrate_Dataset_head', package = 'phosphocie')
+# phosphosite <- read_phosphosite(phosphosite_path)
+
+test_that("read_phosphosite reads successfully", {
+  phosphosite_read <- read_phosphosite(phosphosite_path)
+  phosphosite_human_read <- phosphosite_read %>% dplyr::filter(organism == 'human')
+  testthat::expect_equal(dim(phosphosite_human_read), c(25L, 14L))
+})
+
+test_that("read_kinsub reads successfully", {
+  kinsub_read <- read_kinsub(kinsub_path)
+  kinsub_human_read <- kinsub_read %>% dplyr::filter(organism == 'human' & kin_organism == 'human')
+  testthat::expect_equal(dim(kinsub_human_read), c(30L, 18L))
+})
